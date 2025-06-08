f548b024456d7d62406e57634681787764f9891b
Haruhito-Saito <appledaisukinaonzin@gmail.com>
2025-06-08 15:57:10 +0900

    てすと

---
+++ travelmap/ModalViewSwiftUI.swift
@@ -0,0 +1,18 @@
+//
+//  modalViewSwiftUI.swift
+//  travelmap
+//
+//  Created by 齋藤温仁 on 2025/05/25.
+//
+
+import SwiftUI
+
+struct modalViewSwiftUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    modalViewSwiftUI()
+}
---
+++ travelmap/SheetViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SheetViewController.swift
+//  travelmap
+//
+//  Created by 齋藤温仁 on 2025/05/11.
+//
+
+import Foundation
